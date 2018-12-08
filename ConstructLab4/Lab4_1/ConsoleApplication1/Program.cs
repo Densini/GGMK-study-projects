d0faaf2e2f720a6e07c297676c5662e0ba86d11c
Densini <sinicki@uoggmk.by>
2018-12-08 19:54:48 +0300

    Lab4

---
+++ ConstructLab4/Lab4_1/ConsoleApplication1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static int fun(double a, double b)
+        {
+            b = Math.Pow(a - b, 2);
+            return Convert.ToInt16(b);
+        }
+        static int fun(double a, double b, double c)
+        {
+            c = Math.Pow(a-b, 2);
+            return Convert.ToInt16(c);
+        }
+        static void Main(string[] args)
+        {
+            int a, b, c = 0;
+            Console.Write("Введите значение переменной A = ");
+            a = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Введите значение переменной B = ");
+            b = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Первый метод = "+fun(a, b));
+            Console.WriteLine("Второй метод = "+fun(a, b, c));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConstructLab4/Lab4_2/Lab4_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab4_2
+{
+    class Program
+    {
+        static int fun(string a)
+        {
+            int a1 = 0, a2 = 0;
+            for (int i = 0; i < 3; i++)
+                a1 += Convert.ToInt16(a[i]);
+            for (int i = 0; i < 3; i--)
+                a2 += Convert.ToInt16(a[i]);
+            if (a1 == a2)
+                return Convert.ToInt16(a);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Список счастливых билетов:");
+            for (int i = 100000; i < 1000000; i++)
+            {
+                string a = Convert.ToString(i);
+                Console.WriteLine(fun(a));
+            }
+        }
+    }
+}
