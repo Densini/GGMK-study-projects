711f92d1bebf7d5067a22fc117bca858a29b963b
Densini <sinicki@uoggmk.by>
2018-12-08 18:38:01 +0300

    Lab1

---
+++ ConstructLab1/Lab1_1/Lab1_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lab1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            double s, z;
+            i = 3;
+            s = 5.5;
+            z = s / i;
+            Console.WriteLine("i=" + i);
+            Console.WriteLine("s=" + s);
+            Console.WriteLine("z={0:0.000}", z);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConstructLab1/Lab1_2/Lab1_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Специальность: Обработка деловой и экономической информации");
+            Console.WriteLine("Группа: ПК-22");
+            Console.WriteLine("Фамилия: Синицкий");
+            Console.WriteLine("Имя: Даниил");
+            Console.WriteLine("Отчество: Викторович");
+            Console.ReadLine();
+        }
+    }
+}
