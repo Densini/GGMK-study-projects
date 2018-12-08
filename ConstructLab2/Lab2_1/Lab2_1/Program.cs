335337d17f817700311bc16fbcad4b6ade9801c3
Densini <sinicki@uoggmk.by>
2018-12-08 19:33:03 +0300

    Lab2

---
+++ ConstructLab2/Lab2_1/Lab2_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab2_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            double s, r, h, v;
+            Console.Write("Введите радиус R = ");
+            r = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите высоту H = ");
+            h = Convert.ToDouble(Console.ReadLine());
+            s = 3.14 * r * 2;
+            v = s * h;
+            Console.WriteLine("Площадь основания цилиндра = " + s);
+            Console.Write("Объем цилиндра = " + h);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConstructLab2/Lab2_2/Lab2_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, z, y, b;
+            Console.Write("Введите координату X = ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите координату Y = ");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите координату Z = ");
+            z = Convert.ToDouble(Console.ReadLine());
+            b = x * (Math.Tan(z) + (2 * Math.Cos(y)));
+            Console.Write("Результат вычисления по формуле = {0:0.000}", b);
+            Console.ReadKey();
+        }
+    }
+}
